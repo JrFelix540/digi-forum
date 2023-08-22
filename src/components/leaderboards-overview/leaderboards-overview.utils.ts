2b256377d687ad75e7e6a22051fb9c057cb5722e
JrFelix540 <juniorfelix8825@outlook.com>
2023-08-22 20:32:05 +0300

    ✨ (feat): Adds leaderboards page

---
+++ src/components/leaderboards-overview/leaderboards-overview.utils.ts
@@ -0,0 +1,53 @@
+export interface LeaderboardItem {
+  id: number;
+  firstname: string;
+  lastname: string;
+  username: string;
+  points: number;
+  avatarURL?: string;
+}
+
+export const leaderboardItems: Array<LeaderboardItem> = [
+  {
+    id: 12,
+    firstname: "Ebayyou",
+    lastname: "Anggoro",
+    points: 160,
+    username: "ebayyouanggoro.e@gmail.com",
+  },
+  {
+    id: 43,
+    firstname: "Ebayyou",
+    lastname: "Anggoro",
+    points: 140,
+    username: "ebayyouanggoro.e@gmail.com",
+  },
+  {
+    id: 3,
+    firstname: "Ebayyou",
+    lastname: "Anggoro",
+    points: 100,
+    username: "ebayyouanggoro.e@gmail.com",
+  },
+  {
+    id: 4,
+    firstname: "Ebayyou",
+    lastname: "Anggoro",
+    points: 80,
+    username: "ebayyouanggoro.e@gmail.com",
+  },
+  {
+    id: 6,
+    firstname: "Ebayyou",
+    lastname: "Anggoro",
+    points: 60,
+    username: "ebayyouanggoro.e@gmail.com",
+  },
+  {
+    id: 244,
+    firstname: "Ebayyou",
+    lastname: "Anggoro",
+    points: 160,
+    username: "ebayyouanggoro.e@gmail.com",
+  },
+];
