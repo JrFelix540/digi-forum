787bc60bac18a2d5264f8be3a3b225fa7ab147d4
JrFelix540 <juniorfelix8825@outlook.com>
2023-10-02 10:05:12 +0300

    ✨ (feat): Adds button stories

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./button";
+
+const meta = {
+  title: "components/button",
+  component: Button,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  render: (args) => <Button {...args}>Boom!</Button>,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+export const Primary: StoryObj<typeof meta> = {
+  args: {
+    variant: "primary",
+  },
+};
+
+export const Secondary: StoryObj<typeof meta> = {
+  args: {
+    variant: "secondary",
+  },
+};
+
+export const Tertiary: StoryObj<typeof meta> = {
+  args: {
+    variant: "tertiary",
+  },
+};
