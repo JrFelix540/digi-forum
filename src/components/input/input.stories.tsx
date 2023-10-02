ed9e5c33ffefdc0735e25eaddf4a41e5d2ab55f4
JrFelix540 <juniorfelix8825@outlook.com>
2023-10-02 10:35:36 +0300

    ✨ (feat): Adds input stories

---
+++ src/components/input/input.stories.tsx
@@ -0,0 +1,57 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Input } from "./input";
+
+const meta = {
+  title: "Components/Input",
+  component: Input,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+
+export const Default: StoryObj<typeof meta> = {
+  args: {
+    name: "name",
+    value: "",
+  },
+};
+
+export const OutlinedInput: StoryObj<typeof meta> = {
+  args: {
+    name: "outlined",
+    value: "",
+    variant: "outlined",
+  },
+};
+
+export const InputWithLabel: StoryObj<typeof meta> = {
+  args: {
+    name: "input",
+    value: "",
+    label: "Input Label",
+  },
+};
+
+export const InputWithLabelLink: StoryObj<typeof meta> = {
+  args: {
+    name: "password",
+    value: "",
+    label: "Password",
+    labelLink: {
+      name: "Forgot password?",
+      url: "#",
+    },
+  },
+};
+
+export const InputWithPlaceholder: StoryObj<typeof meta> = {
+  args: {
+    name: "Password",
+    value: "",
+    label: "Password",
+    placeholder: "Fill in Password",
+  },
+};
