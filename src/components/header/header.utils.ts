1f1c39c72f09634808a78fc8a639f899ac4e83e0
JrFelix540 <juniorfelix8825@outlook.com>
2023-10-02 03:15:19 +0300

    ✨ (feat): Adds navigation to header

---
+++ src/components/header/header.utils.ts
@@ -0,0 +1,22 @@
+export const headerLinks: Array<{ link: string; name: string }> = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "About",
+    link: "/about",
+  },
+  {
+    name: "Threads",
+    link: "/threads",
+  },
+  {
+    name: "Community",
+    link: "/community",
+  },
+  {
+    name: "Leaderboards",
+    link: "/leaderboards",
+  },
+];
---
+++ src/components/notification-icon/notification-icon.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export const NotificationIcon = () => {
+  return (
+    <Link href="#" className="relative">
+      <div className="bg-brand-primary p-2 rounded-full flex items-center justify-center relative z-10">
+        <Image
+          src="/notification.png"
+          alt="notifications"
+          height={24}
+          width={24}
+        />
+      </div>
+      <div className="w-full h-full bg-comment-bg rounded-full absolute -top-0.5 -left-0.5" />
+    </Link>
+  );
+};
