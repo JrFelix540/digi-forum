d5489f335a168c6473b074d88015619de5373b3d
JrFelix540 <juniorfelix8825@outlook.com>
2023-10-05 14:37:20 +0300

    ✨ (feat): Adds avatar story

---
+++ src/components/avatar/avatar.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Avatar as AvatarComponent } from "./avatar";
+
+const meta = {
+  title: "Components/Avatar",
+  component: AvatarComponent,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof AvatarComponent>;
+
+export default meta;
+
+export const Small: StoryObj<typeof AvatarComponent> = {
+  args: {
+    size: "sm",
+  },
+};
+
+export const Large: StoryObj<typeof AvatarComponent> = {
+  args: {
+    size: "lg",
+  },
+};
+
+export const SmallSquare: StoryObj<typeof AvatarComponent> = {
+  args: {
+    size: "sm",
+    square: true,
+  },
+};
+
+export const LargeSquare: StoryObj<typeof AvatarComponent> = {
+  args: {
+    size: "lg",
+    square: true,
+  },
+};
